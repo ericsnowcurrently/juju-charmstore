59e1ad557e7a224e24d92c7c20e421305e54a5db
Eric Snow <ericsnowcurrently@gmail.com>
2016-03-07 11:31:49 -0700

    Implement serveDownloadResource (incomplete).

--- internal/v5/resources.go
+++ internal/v5/resources.go
@@ -4,10 +4,17 @@
 package v5 // import "gopkg.in/juju/charmstore.v5-unstable/internal/v5"
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"path"
+	"strconv"
+	"strings"
+	"time"
 
 	"gopkg.in/errgo.v1"
+	"gopkg.in/juju/charm.v6-unstable/resource"
 	"gopkg.in/juju/charmrepo.v2-unstable/csclient/params"
 
 	"gopkg.in/juju/charmstore.v5-unstable/internal/mongodoc"
@@ -41,7 +48,55 @@ func (h *ReqHandler) serveResources(id *router.ResolvedURL, w http.ResponseWrite
 }
 
 func (h *ReqHandler) serveDownloadResource(id *router.ResolvedURL, w http.ResponseWriter, req *http.Request) error {
-	return errNotImplemented
+	_, err := h.AuthorizeEntityAndTerms(req, []*router.ResolvedURL{id})
+	if err != nil {
+		return errgo.Mask(err, errgo.Any)
+	}
+	name, revision, err := extractResourceRequest(req)
+	if err != nil {
+		return errgo.Mask(err, errgo.Any)
+	}
+	res, reader, err := h.Store.OpenResourceBlob(id, name, revision)
+	if err != nil {
+		return errgo.Mask(err, errgo.Is(params.ErrNotFound))
+	}
+	defer reader.Close()
+	h.sendResource(id, res, w, req, reader)
+	return nil
+}
+
+func extractResourceRequest(req *http.Request) (name string, revision int, err error) {
+	reqPath := strings.TrimPrefix(path.Clean(req.URL.path), "/")
+	parts := strings.SplitN(reqPath, "/", 2)
+	name = parts[0]
+	revision = -1 // "use the latest"
+	if len(parts) == 2 {
+		rev, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return "", 0, errgo.Notef(err, "invalid resource revision in URL")
+		}
+		revision = rev
+	}
+	return name, revision, nil
+}
+
+func (h *ReqHandler) sendResource(id *router.ResolvedURL, res resource.Resource, w http.ResponseWriter, req *http.Request, reader io.ReadCloser) {
+	header := w.Header()
+	// TODO(ericsnow) Use a separate resource cache control?
+	setArchiveCacheControl(w.Header(), h.isPublic(id))
+	hash := res.Fingerprint.String()
+	logger.Infof("sendResource setting %s=%s", params.ContentHashHeader, hash)
+	header.Set(params.ContentHashHeader, hash)
+	header.Set(params.EntityIdHeader, id.PreferredURL().String())
+	resID := fmt.Sprintf("%s/%d", res.Name, res.Revision)
+	header.Set(params.ResourceIdHeader, resID)
+
+	// TODO(ericsnow) track stats for resource downloads?
+
+	// TODO(rog) should we set connection=close here?
+	// See https://codereview.appspot.com/5958045
+	timestamp := time.Time{}
+	http.ServeContent(w, req, res.Path, timestamp, reader)
 }
 
 func (h *ReqHandler) serveUploadResource(id *router.ResolvedURL, w http.ResponseWriter, req *http.Request) error {
